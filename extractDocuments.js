8707d67125070349015cb5c57ad19e33f4371536
Salvador de la Puente González <salva@unoyunodiez.com>
2019-09-08 10:01:05 +0200

    Finish document separator

--- extractDocuments.js
+++ extractDocuments.js
@@ -1,31 +1,85 @@
 const NAMED_DOCUMENT_MARK = /^---(.+)\n/gm;
+const ANONYMOUS_DELIMITER = '---\n';
+const NO_NAME = '';
+const END_OF_FILE = {};
+
+class FMDocumentFinder {
+  constructor(source) {
+    this._source = source;
+    this._startIndex = 0;
+    this._documentName = NO_NAME;
+    this._documentDelimiter = new RegExp(NAMED_DOCUMENT_MARK);
+    this._finder = new RegExp(NAMED_DOCUMENT_MARK);
+    this._done = false;
+  }
+
+  [Symbol.iterator]() {
+    return this;
+  }
+
+  next() {
+    if (this._done) { return { done: true }; }
+    return {
+      done: false,
+      value: this._findNextDocument()
+    };
+  }
+
+  _findNextDocument() {
+    const nextDocPointer = this._locateNextDocument();
+    if (nextDocPointer === END_OF_FILE) {
+      return this._getLastDocument();
+    }
+
+    const document =
+      this._getDocumentEndingAt(nextDocPointer.startIndex);
+    this._advanceSearchTo(nextDocPointer);
+
+    const anonymousDocIsEmpty = document.name === NO_NAME && !document.content;
+    if (anonymousDocIsEmpty) {
+      return this._findNextDocument();
+    }
+
+    return document;
+  }
+
+  _locateNextDocument() {
+    const match = this._finder.exec(this._source);
+    return !match ? END_OF_FILE : {
+      name: match[1],
+      startIndex: match.index
+    };
+  }
+
+  _getLastDocument() {
+    const document = {
+      name: this._documentName,
+      content: this._extractDocContent(this._startIndex, this._source.length)
+    };
+    this._done = true;
+    return document;
+  }
+
+  _getDocumentEndingAt(endIndex) {
+    return {
+      name: this._documentName,
+      content: this._extractDocContent(this._startIndex, endIndex)
+    };
+  }
+
+  _extractDocContent(startIndex, endIndex) {
+    return this._source
+      .substring(startIndex, endIndex)
+      .replace(this._documentDelimiter, ANONYMOUS_DELIMITER)
+      .trim();
+  }
+
+  _advanceSearchTo({ name, startIndex }) {
+    this._documentName = name;
+    this._startIndex = startIndex;
+  }
+}
 
 module.exports = function (source) {
-  const mark = new RegExp(NAMED_DOCUMENT_MARK);
-
-  function extract(startIndex, endIndex) {
-    const mark = new RegExp(NAMED_DOCUMENT_MARK);
-    return source.substring(startIndex, endIndex).trim().replace(mark, "---\n");
-  }
-
-  const result = [];
-  let match = mark.exec(source);
-  let currentKey = '';
-  if (match && match.index == 0) {
-    currentKey = match[1];
-    match = mark.exec(source);
-  }
-  let startIndex = 0;
-  let endIndex = null;
-  let nextKey = null;
-  while (match) {
-    nextKey = match[1];
-    endIndex = match.index;
-    result.push([currentKey, extract(startIndex, endIndex)]);
-    startIndex = endIndex;
-    currentKey = nextKey;
-    match = mark.exec(source);
-  }
-  result.push([currentKey, extract(startIndex, source.length)]);
-  return result;
+  return [...new FMDocumentFinder(source)]
 }
--- test/extractDocuments.test.js
+++ test/extractDocuments.test.js
@@ -1,6 +1,4 @@
 import ExtractDocuments from "../extractDocuments";
-import { isTemplateElement, exportAllDeclaration } from "@babel/types";
-import M from "minimatch";
 
 let result;
 
@@ -29,14 +27,14 @@ describe("extractDocuments", () => {
   it("recognizes a regular frontmatter document", () => {
     extractDocuments(anonymousDoc);
     expect(result).toHaveLength(1);
-    const [[name, content]] = result;
+    const [{ name, content }] = result;
     expect(name).toEqual("");
     expect(content).toEqual(anonymousDoc);
   });
 
   it("recognizes a named frontmatter document", () => {
     const named = nameddoc("doc1");
-    const [[name, content]] = extractDocuments(named);
+    const [{ name, content }] = extractDocuments(named);
     expect(name).toEqual("doc1");
     expect(content).toEqual(anondoc());
   })
@@ -48,7 +46,7 @@ describe("extractDocuments", () => {
     const severalDocuments = documents.join("\n\n");
     extractDocuments(severalDocuments);
     expect(result).toHaveLength(indices.length);
-    result.forEach(([name, content], index) => {
+    result.forEach(({ name, content }, index) => {
       expect(name).toEqual(docNames[index]);
       expect(content).toEqual(anonymousDoc);
     });
@@ -60,10 +58,10 @@ ${nameddoc('doc1')}`;
     extractDocuments(mixedDocs);
     expect(result).toHaveLength(2);
     const [doc1, doc2] = result;
-    expect(doc1[0]).toEqual("");
-    expect(doc2[0]).toEqual("doc1");
-    expect(doc1[1]).toEqual(anonymousDoc);
-    expect(doc2[1]).toEqual(anonymousDoc);
+    expect(doc1.name).toEqual("");
+    expect(doc2.name).toEqual("doc1");
+    expect(doc1.content).toEqual(anonymousDoc);
+    expect(doc2.content).toEqual(anonymousDoc);
   });
 
   it("does not recognizes an anonymous after the first named one", () => {
@@ -71,7 +69,7 @@ ${nameddoc('doc1')}`;
 ${anonymousDoc}`;
     extractDocuments(mixedDocs);
     expect(result).toHaveLength(1);
-    const [[name, content]] = result;
+    const [{ name, content }] = result;
     expect(name).toEqual("doc1")
     expect(content).toEqual([anonymousDoc, anonymousDoc].join('\n'))
   });
@@ -88,7 +86,7 @@ ${nameddoc("doc1")}
 `;
     extractDocuments(docsWithBlanks);
     expect(result).toHaveLength(2);
-    result.forEach(([_, content]) => {
+    result.forEach(({ content }) => {
       expect(content).toEqual(anonymousDoc);
     })
   });
@@ -105,9 +103,21 @@ ${anonymousDoc}
 `;
     extractDocuments(docsWithBlanks);
     expect(result).toHaveLength(1);
-    expect(result[0][0]).toEqual('');
-    expect(result[0][1]).toEqual(`${anonymousDoc}
+    expect(result[0].name).toEqual('');
+    expect(result[0].content).toEqual(`${anonymousDoc}
 
-  ${nameddoc("doc1")}`)
+  ${nameddoc("doc1")}`);
+  });
+
+  it("cannot recognazie content-only named docs", () => {
+    const docWithNoYaml = `---noYaml
+This is content.`;
+    const secondDocWithNoYAML = `${anonymousDoc}
+${docWithNoYaml}
+`;
+    extractDocuments(secondDocWithNoYAML);
+    expect(result).toHaveLength(2);
+    expect(result[1].name).toEqual('noYaml');
+    expect(result[1].content).not.toEqual(`This is content.`);
   });
 });
